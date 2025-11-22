28febc2a958093e981433fba0764b5b1e2724356
marcoAntaniom <milancamarco694@gmail.com>
2025-11-22 02:22:37 -0300

    Método Ingresar Venta.

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Proyecto de Funciones y Matrices
 #### Marco Milanca
 - Se crea la Conexión con la Base de Datos.
 - Se arreglan errores en el script SQL.
+
+### 22 De Noviembre 2025 (Madrugada)
+#### Marco Milanca
+- Se agrega un .gitignore para ignorar todas las carpetas del caché de Python.
+- Se deja funcionando parcialmente el método ingresar_venta.
---
+++ models/ventas.py
@@ -0,0 +1,29 @@
+from config.conexion import ConexionDB
+import datetime
+
+class Ventas:
+
+    folio: int
+    rut_vendedor: str
+    fecha_venta: str
+    estado_venta: int
+    documento_id: int
+    total: int
+
+    def ingresar_venta(self):
+        try:
+            total_inicial = 0
+            fecha = datetime.date.today() # Se obtiene la fecha de hoy.
+            c = ConexionDB()
+            sql = """INSERT INTO ventas (folio, rut_vendedor, fecha_venta, estado_venta_id, documento_id, total_venta) 
+                        VALUES (:folio, :rut, :fecha, 1, 1, :total)"""
+
+            c.cursor.execute(sql, folio=self.folio, rut=self.rut_vendedor, fecha=fecha, total=total_inicial)
+            c.conexion.commit()
+
+        except Exception as e:
+            print(f"Error en la BD: {e}")
+        finally:
+            if c: c.cursor.close()
+            if c: c.conexion.close()
+        
