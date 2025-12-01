cb7b6f2214db1c51e6e20f41be453dc62fc48e21
Juaquin-35 <juacojj201@gmail.com>
2025-12-01 16:08:55 -0300

    Update conexion.py

--- config/conexion.py
+++ config/conexion.py
@@ -5,6 +5,8 @@ class ConexionDB:
     def __init__(self):
         user = "INACAP"
         password = "inacap"
+        user = "SISTEMA_VENTAS"
+        password = "ventas"
         dsn = "localhost/xe" # Cambiar en caso de que tengan localhost o otra IP.
 
         self.conexion = oracledb.connect(user=user, password=password, dsn=dsn)
